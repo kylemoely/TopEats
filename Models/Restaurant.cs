db8bfb99768910e3abfc8404454c257f5eb921bb
Kyle Moellenkamp <kylemoely@gmail.com>
2024-08-17 00:54:46 -0600

    Added a comment

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,7 +1,7 @@
 
 public class Restaurant
 {
-    public int restaurantId { get; set; }
+    public int restaurantId { get; set; } //  PRIMARY KEY
     public string restaurantName { get; set; }
     public string cuisine { get; set; }
     public int priceCategory { get; set; }
