f83453515d4a34a97fa6d5a904e231dd00492380
Kyle Moellenkamp <kylemoely@gmail.com>
2024-08-22 18:45:26 -0600

    Added  to model files

--- Models/ReviewLike.cs
+++ Models/ReviewLike.cs
@@ -1,4 +1,5 @@
 using System;
+using TopEats.Services;
 
 namespace TopEats.Models
 {
--- obj/Debug/net8.0/TopEats.AssemblyInfo.cs
+++ obj/Debug/net8.0/TopEats.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopEats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95250be7cb0734f7dee6096e75e8347dc3ae4bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b07280f72da0ea2cb17337f2844dbe6b56ca005")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopEats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopEats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
