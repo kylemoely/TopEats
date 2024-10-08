14f1cd705d51d8f913318a7e83cd9d8f62c8d149
Kyle Moellenkamp <kylemoely@gmail.com>
2024-10-07 18:33:11 -0600

    added initial user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TopEats.Models;
+using TopEats.Services;
+
+namespace TopEats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsers();
+            return Ok(users);
+        }
+
+        // GET: api/User/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var user = await _userService.GetUserById(id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<ActionResult> CreateUser([FromBody] User newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _userService.CreateUser(newUser);
+
+            return CreatedAtAction(nameof(GetUserById), new { id = newUser.userId }, newUser);
+        }
+
+        // PUT: api/User/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            if (id != updatedUser.Id || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userService.UpdateUser(updatedUser);
+
+            return NoContent();
+        }
+
+        // DELETE: api/User/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userService.DeleteUser(id);
+
+            return NoContent();
+        }
+    }
+}
