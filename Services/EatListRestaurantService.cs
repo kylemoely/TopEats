7628976b386a7ba4e601d8c2b1f29cb2ccd2416d
kylemoely <kylemoely@gmail.com>
2024-10-09 14:41:05 -0600

    Create EatListRestaurantService.cs

---
+++ Services/EatListRestaurantService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TopEats.Models;
+using TopEats.Repositories;
+
+namespace TopEats.Services
+{
+    public class EatListRestaurantService : IEatListRestaurantService
+    {
+        private readonly IEatListRestaurantRepository _eatListRestaurantRepository;
+
+        public EatListRestaurantService(IEatListRestaurantRepository eatListRestaurantRepository)
+        {
+            _eatListRestaurantRepository = eatListRestaurantRepository;
+        }
+
+        public async Task<IEnumerable<EatListRestaurant>> GetEatListRestaurants(int eatListId)
+        {
+            return await _eatListRestaurantRepository.GetEatListRestaurants(eatListId);
+        }
+    }
+}
