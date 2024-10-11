2b6f0cb88fdf2e6bab025e00998cd3890e91fc16
kylemoely <kylemoely@gmail.com>
2024-10-11 12:08:02 -0600

    Update IFollowRepository.cs

--- Repositories/IFollowRepository.cs
+++ Repositories/IFollowRepository.cs
@@ -9,5 +9,6 @@ namespace TopEats.Repositories
     {
         Task<IEnumerable<Follow>> GetUserFollowers(int userId);
         Task<IEnumerable<Follow>> GetUserFollowees(int userId);
+        Task CreateFollow(int followerId, int followeeId);
     }
 }
