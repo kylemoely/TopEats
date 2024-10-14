adbce9ddfb6859893bae895d8c0d3358d27039cd
kylemoely <kylemoely@gmail.com>
2024-10-14 10:07:53 -0600

    Update IUserFavRestaurantRepository.cs

--- Repositories/IUserFavRestaurantRepository.cs
+++ Repositories/IUserFavRestaurantRepository.cs
@@ -9,5 +9,6 @@ namespace TopEats.Repositories
     {
         Task<IEnumerable<UserFavRestaurant>> GetUserTopRestaurants(int userId);
         Task CreateUserTopRestaurant(UserFavRestaurant userFavRestaurant);
+        Task UpdateUserTopRestaurant(UserFavRestaurant userFavRestaurant);
     }
 }
