06a69f70c5f353b10f19012a3c4bce4080b2db77
kylemoely <kylemoely@gmail.com>
2024-10-14 16:30:16 -0600

    Create CommentLikeController.cs

---
+++ Controllers/CommentLikeController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TopEats.Models;
+using TopEats.Services;
+
+namespace TopEats.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentLikeController : ControllerBase
+    {
+        private readonly ICommentLikeService _commentLikeService;
+
+        public CommentLikeController(ICommentLikeService commentLikeService)
+        {
+            _commentLikeService = commentLikeService;
+        }
+
+        // GET: api/CommentLike/{commentId}
+        [HttpGet("{commentId}")]
+        public async Task<ActionResult<IEnumerable<CommentLike>>> GetCommentLikes(int commentId)
+        {
+            var commentLikes = await _commentLikeService.GetCommentLikes(commentId);
+
+            if (commentLikes == null)
+            {
+                return NotFound( new { message = $"Comment not found."} );
+            }
+
+            return Ok(commentLikes);
+        }
+
+        // POST: api/CommentLike
+        [HttpPost]
+        public async Task<ActionResult> CreateCommentLike([FromBody] CommentLike commentLike)
+        {
+            if (commentLike == null)
+            {
+                return BadRequest("Request body is null.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("CommentLike data is invalid.");
+            }
+
+            await _commentLikeService.CreateCommentLike(commentLike);
+
+            // Return a 201 Created response with the created comment like information
+            return CreatedAtAction(nameof(GetCommentLikes), new { commentId = commentLike.commentId }, commentLike);
+        }
+
+        // DELETE: api/CommentLike
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCommentLike([FromBody] CommentLike commentLike)
+        {
+            if (commentLike == null)
+            {
+                return BadRequest("Request body is null.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("CommentLike data is invalid.");
+            }
+
+            await _commentLikeService.DeleteCommentLike(commentLike);
+
+            return NoContent(); // 204 No Content response for successful deletion
+        }
+    }
+}
