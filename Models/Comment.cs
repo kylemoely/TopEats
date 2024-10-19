4487543eb72e686550ef4f0d4c26d962249218b1
Kyle Moellenkamp <kylemoely@gmail.com>
2024-10-18 22:21:03 -0600

    added json serialization helpers

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,38 +1,44 @@
 using System;
 using TopEats.Services;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TopEats.Models
 {
     public class Comment
     {
-        public int? commentId { get; set; } // PRIMARY KEY
-        public int reviewId { get; set; } // FOREIGN KEY REFERENCES REVIEWS
-        public int userId { get; set; } // FOREIGN KEY REFERENCES USERS
-        public string commentText { get; set; } 
+        public int CommentId { get; set; } // PRIMARY KEY
+        public int ReviewId { get; set; } // FOREIGN KEY REFERENCES REVIEWS
+        public int UserId { get; set; } // FOREIGN KEY REFERENCES USERS
+        public string CommentText { get; set; } 
 
+        [ValidateNever]
+        [JsonIgnore]
         public Review AssignedReview { get; set; }
+        [ValidateNever]
         public User AssignedUser { get; set; }
 
-        public Comment(int _commentId, int _reviewId, int _userId, string _commentText)
+        [JsonConstructor]
+        public Comment(int commentId, int reviewId, int userId, string commentText)
         {
-            commentId = _commentId;
-            reviewId = _reviewId;
-            userId = _userId;
-            commentText = _commentText;
+            CommentId = commentId;
+            ReviewId = reviewId;
+            UserId = userId;
+            CommentText = commentText;
         }
 
-        public Comment(int _reviewId, int _userId, string _commentText)
+        public Comment(int reviewId, int userId, string commentText)
         {
-            reviewId = _reviewId;
-            userId = _userId;
-            commentText = _commentText;
+            ReviewId = reviewId;
+            UserId = userId;
+            CommentText = commentText;
         }
 
         public async Task AssignUserAndReview(IUserService userService, IReviewService reviewService)
         {
-            AssignedReview = await reviewService.GetReviewById(reviewId);
-            AssignedUser = await userService.GetUserById(userId);
+            AssignedReview = await reviewService.GetReviewById(ReviewId);
+            AssignedUser = await userService.GetUserById(UserId);
         }
     }
 }
