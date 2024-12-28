d80729f96f824b52031ef610a0a2ddecc8cf8583
Kyle Moellenkamp <kylemoely@gmail.com>
2024-12-27 20:46:23 -0700

    Added json stuff to reviewLike

--- Models/ReviewLike.cs
+++ Models/ReviewLike.cs
@@ -1,6 +1,8 @@
 using System;
 using TopEats.Services;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TopEats.Models
 {
@@ -9,9 +11,12 @@ namespace TopEats.Models
         public Guid ReviewId { get; set; } // FOREIGN KEY REFERENCES REVIEWS
         public Guid UserId { get; set; } // FOREIGN KEY REFERENCES USERS
 
-        public UserDTO AssignedUser { get; set; }
-        public Review AssignedReview { get; set; }
+        [JsonIgnore]
+        public UserDTO? AssignedUser { get; set; }
+        [JsonIgnore]
+        public Review? AssignedReview { get; set; }
 
+        [JsonConstructor]
         public ReviewLike(Guid reviewId, Guid userId)
         {
             ReviewId = reviewId;
